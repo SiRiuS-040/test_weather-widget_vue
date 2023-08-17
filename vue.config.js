7a0e272bf7243c3fe2f0955a0b625a9d4dfd6206
voskresenskiy.v <vladislav.voskresenskiy@gmail.com>
2023-08-17 14:40:32 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/test_weather-widget_vue/'
+      : '/'
 })
 
 const webpack = require('webpack')
